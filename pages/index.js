adf1a07317f95834bc4d1631314b40ca7426797a
Matias Fernandez <mfernandez@techinsights.com>
2020-10-26 20:38:42 -0300

    Updating the version.

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! V3</a>
+          Welcome to <a href="https://nextjs.org">Next.js! V4</a>
         </h1>
 
         <p className={styles.description}>

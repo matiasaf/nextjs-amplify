2f7e32c30283231ce7b602b50d9f464e2047ef4e
Matias Fernandez <mfernandez@techinsights.com>
2021-08-20 13:38:17 -0300

    Modified background gradient on the main section

--- components/styled/backgroundImage.js
+++ components/styled/backgroundImage.js
@@ -1,6 +1,10 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
 export const BackgroundImage = styled.div`
-    background-image: url('/images/background.jpeg');
-    background-size: cover;
+  background-image: linear-gradient(to right, 
+      rgba(30, 30, 30, 0.85),
+      rgba(90, 90, 90, 0.85)
+    ),
+    url("/images/background.jpeg");
+  background-size: cover;
 `;
